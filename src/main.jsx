76cee0051584b69439101d054f9696edcde94280
miracleSw <aryaanopro@gmail.com>
2025-10-06 22:47:32 +0700

    Quiz

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './App.css'
 import './components/quiz.css'
@@ -8,6 +9,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Quiz">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
